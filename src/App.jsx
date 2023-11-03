11e06bd782d7c2f5eb5aff160f417cfc9a7e6cf2
Krathish K <krathishs.k123@gmail.com>
2023-11-03 13:04:50 +0530

    react useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function changeCount(process) {
+    if (process === -1) {
+      setCount((prev) => prev - 1);
+    } else if (process === 1) {
+      setCount((prev) => prev + 1);
+    } else setCount(0);
+  }
 
   return (
-    <>
+    <div className="useState">
+      <h2>React useState Hook</h2>
+      <p>{count}</p>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => changeCount(1)}>Increment Count</button>
+        <button onClick={() => changeCount(-1)}>Decrement Count</button>
+        <button onClick={() => changeCount(0)}>Reset Count</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
